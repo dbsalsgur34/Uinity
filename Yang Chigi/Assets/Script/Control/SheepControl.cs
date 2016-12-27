51027431219b7fbba71f62dd8ff736e7675d45bf
DESKTOP-8O4TTH0\ak569 <lisencej@gmail.com>
2016-12-27 22:15:27 +0900

    2016/12/27 제작

---
+++ Yang Chigi/Assets/Script/Control/SheepControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepControl : MonoBehaviour {
+
+    public GameObject leader;
+    public GameObject Master;
+    public Rigidbody2D rb;
+    public int thrust;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+    }
+}
