521e0c8b2792ec9c138746549ef44833f571c29d
DESKTOP-8O4TTH0\ak569 <lisencej@gmail.com>
2016-12-27 21:29:56 +0900

    no message

---
+++ Yang Chigi/Assets/Script/HeadControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadControl : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            rb.AddForce(transform.up * thrust);
+        if (Input.GetKey(KeyCode.DownArrow))
+            rb.AddForce(transform.up * -thrust);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.AddForce(transform.right * -thrust);
+        if (Input.GetKey(KeyCode.RightArrow))
+            rb.AddForce(transform.right * thrust);
+    }
+}
