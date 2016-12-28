6e92eff82e281948327d4d86f6616a80567ffdb9
DESKTOP-8O4TTH0\ak569 <lisencej@gmail.com>
2016-12-28 19:55:32 +0900

    2016/12/28 양이 양치기를 따라다니는 것까지는 완료

--- Yang Chigi/Assets/Script/Control/HeadControl.cs
+++ Yang Chigi/Assets/Script/Control/HeadControl.cs
@@ -4,23 +4,59 @@ using UnityEngine;
 
 public class HeadControl : MonoBehaviour {
 
-    public float thrust;
-    public Rigidbody2D rb;
+    public Vector3 LookDirection;
 
-    void Start()
+    public float Maxspeed;
+    public float Accel;
+
+    public float speed;
+
+    public float xx;
+    public float yy;
+
+    public Vector2 towardvector;
+    public Rigidbody2D Target;
+
+    private void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        Target = this.GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void KeyboardInput()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
-            rb.AddForce(transform.up * thrust);
-        if (Input.GetKey(KeyCode.DownArrow))
-            rb.AddForce(transform.up * -thrust);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            rb.AddForce(transform.right * -thrust);
+        xx = Input.GetAxisRaw("Horizontal");
+        yy = Input.GetAxisRaw("Vertical");
+
         if (Input.GetKey(KeyCode.RightArrow))
-            rb.AddForce(transform.right * thrust);
+        {
+            LookDirection = xx * Vector3.forward;
+            Target.AddForce(Vector2.right * Accel);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            LookDirection = xx * Vector3.forward;
+            Target.AddForce(Vector2.left * Accel);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Target.AddForce(Vector2.up * Accel);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Target.AddForce(Vector2.down * Accel);
+        }
+
+    }
+
+    void LookUpdate()
+    {
+        Quaternion R = Quaternion.LookRotation(LookDirection);
+        transform.rotation = R;
+    }
+
+    private void FixedUpdate()
+    {
+        KeyboardInput();
+        LookUpdate();
     }
 }
--- Yang Chigi/Assets/Script/Control/SheepControl.cs
+++ Yang Chigi/Assets/Script/Control/SheepControl.cs
@@ -2,21 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepControl : MonoBehaviour {
-
-    public GameObject leader;
-    public GameObject Master;
-    public Rigidbody2D rb;
-    public int thrust;
-
+public class SheepControl : MonoBehaviour
+{
+    // 항상 일정 거리를 유지하도록!
+    public GameObject target;
+    public List<Vector3> positions;
+    public int distance_permitted;
+    public float speed;
+    private Vector3 lastLeaderPosition;
+    // Use this for initialization
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        positions.Add(target.transform.position);
     }
 
-    
-	// Update is called once per frame
-	void FixedUpdate () {
-        
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (lastLeaderPosition != positions[positions.Count - 1])
+        {
+            positions.Add(target.transform.position);
+        }
+
+        if (positions.Count >= distance_permitted)
+        {
+            if (gameObject.transform.position != positions[0])
+            {
+
+                transform.position = Vector3.MoveTowards(transform.position, positions[0], Time.deltaTime * speed);
+
+            }
+            else
+            {
+                positions.Remove(positions[0]);
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+        }
+        lastLeaderPosition = target.transform.position;
     }
 }
+
